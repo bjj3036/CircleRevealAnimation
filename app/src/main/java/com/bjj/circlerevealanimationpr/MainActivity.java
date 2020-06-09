3b828f7e8bb80791f4c3b376838899f030e071f7
baejunjae1009 <baejunjae1009@gmail.com>
2020-06-09 09:48:53 +0900

    first commit

---
+++ app/src/main/java/com/bjj/circlerevealanimationpr/MainActivity.java
@@ -0,0 +1,25 @@
+package com.bjj.circlerevealanimationpr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.Animator;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_show).setOnClickListener(v -> {
+            View target = findViewById(R.id.layout_circle_reveal);
+            target.setVisibility(View.VISIBLE);
+            int x = target.getWidth() / 2, y = target.getHeight() / 2;
+            Animator animator = ViewAnimationUtils.createCircularReveal(target, x, y, 0, (float) Math.hypot(x, y));
+            animator.setDuration(1000);
+            animator.start();
+        });
+    }
+}
